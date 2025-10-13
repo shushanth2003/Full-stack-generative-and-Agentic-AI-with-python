9a982e7719f139cc4837e7c4b58c43ca49689395
shushanth2003 <bsshu2003@gmail.com>
2025-10-13 15:17:59 +0530

    Learning about introduction of python

--- python-code/Introduction/01_python_shop.py
+++ python-code/Introduction/01_python_shop.py
@@ -2,10 +2,12 @@ class Chai:
     def __init__(self,sweetness,milklevel):
         self.sweetness=sweetness
         self.milklevel=milklevel
-    def sip(self):
+    @staticmethod
+    def sip():
         print("Add a coffee")
     def add_a_sugar(self,amount):
         print("Add a Sugar",amount)
 
 my_chai=Chai("medium",4)
 my_chai.add_a_sugar(5);
+Chai.sip();
