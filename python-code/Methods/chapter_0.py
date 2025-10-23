16930dc0d83bb470903f3d2a3856a4beee52b74e
shushanth2003 <bsshu2003@gmail.com>
2025-10-23 18:31:57 +0530

    Feat: Learned about Function

---
+++ python-code/Methods/chapter_0.py
@@ -0,0 +1,4 @@
+def customer(name,chai_type):
+    print(f"A customer name is {name} chai type is {chai_type} ");
+customer("shushanth","yellom chai");
+customer("aravind","tea chai");
---
+++ python-code/Methods/chapter_1.py
@@ -0,0 +1,19 @@
+#fetch sales
+def fetch_sales():
+    print("Fetch Sales Data");
+
+#filter valid sales
+def filter_valid_sales():
+    print("Filter valid sales");
+
+#summarize data
+def summarize_data():
+    print("Summarize the data");
+
+#Generate Report
+def generate_report():
+    fetch_sales();
+    filter_valid_sales();
+    summarize_data();
+
+generate_report();
