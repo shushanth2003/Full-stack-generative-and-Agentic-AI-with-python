4c1a15c41f11faf34f0cde9872abd109d224c6ca
shushanth2003 <bsshu2003@gmail.com>
2025-10-24 12:13:01 +0530

    Feat: Learned about Comprehension

---
+++ python-code/comprehension/chapter_0.py
@@ -0,0 +1,8 @@
+menu=[
+    "Parotta",
+    "chicken fried rice",
+    "chicken biryani",
+    "chicken gravi"
+];
+meals_reduced_menu=[tea for tea in menu if "chicken" in tea];
+print(meals_reduced_menu);
